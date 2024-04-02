effbd18e2b232d083fa6eaedf81996d9a16c98a1
suros <suros@PC>
2024-04-02 17:55:58 +0200

    prueba

--- Practica_JavaDoc/src/Liga/Jugadores.java
+++ Practica_JavaDoc/src/Liga/Jugadores.java
@@ -43,7 +43,7 @@ public void imprimirDatosJugador() {
 }
 
 /**
- * Permite actualizar el dorsal del jugador
+ * Permite actualizar el dorsal del jugador.
  */
 
 public void actualizarDorsal(int nuevoDorsal) {
